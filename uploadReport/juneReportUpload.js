3d28a6f3bb8bf27f452ca05f5eb1eb327e8ec001
sidharth1017 <sidharthv605@gmail.com>
2024-07-22 21:57:13 +0530

    Revert "Testing report Page design"
    This reverts commit 368412d50d965ac7cd32d172940b8867ae424a64.

--- uploadReport/juneReportUpload.js
+++ uploadReport/juneReportUpload.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 const axios = require('axios');
 const path = require('path');
 
-const csvFilePath = path.join(__dirname, './16JulyReports.csv');
+const csvFilePath = path.join(__dirname, './24JuneReports .csv');
 const apiEndpoint = 'http://localhost:5500/admin/add/report';
 
 const uploadReport = async (reportData) => {
