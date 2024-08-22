83e24ab667b7879eea189854ba15329421b4d324
sidharth1017 <sidharthv605@gmail.com>
2024-08-22 21:51:28 +0530

    pdf fix

--- client/src/Pages/ReportDetails/ReportDetails.jsx
+++ client/src/Pages/ReportDetails/ReportDetails.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css"; // Import necessary styles
 import { getReportDetails } from "../../Services/Api";
 import "./ReportDetails.css";
 import ReportCardSlider from "../../components/ReportCardSlider/ReportCardSlider";
@@ -29,18 +31,21 @@ const ReportDetails = () => {
             By ~ {report.author} • {report.sector} • {report.subSector}
           </p>
         </div>
-        {/* Add a wrapper around iframe for responsiveness */}
-        <div className="report-frame-wrapper">
-          <iframe
-            className="report-frame"
-            src={report.link}
-            frameBorder="0"
-            scrolling="yes"
-            title="Report PDF"
-          ></iframe>
+
+        {/* Use react-pdf Viewer component */}
+        <div className="pdfViewer">
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
+          >
+            <Viewer
+              fileUrl={report.link}
+              onError={(error) => console.error("Failed to load PDF:", error)}
+            />
+          </Worker>
         </div>
       </div>
-      <div className="simmilarReport">
+
+      <div className="similarReport">
         <h1>Similar Reports</h1>
         <ReportCardSlider reports={report.similarReports} />
       </div>
